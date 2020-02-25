86ca20da1eb48db9dd078cc6175281d1764a6902
Atsushi Yokohama <brave.mont.saint.micheal@gmail.com>
2020-02-26 01:27:32 +0900

    add google adsence code

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,5 +1,14 @@
 module.exports = {
     title: 'BEACHSIDE.DEV',
-    base:'/',
-    dest: 'docs/'
+    base: '/',
+    dest: 'docs/',
+    head: [
+        [
+            "script",
+            {
+                "async src": "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
+                "data-ad-client": "ca-pub-2118922083603689"
+            }
+        ]
+    ],
 }
