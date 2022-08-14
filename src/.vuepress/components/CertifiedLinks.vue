35e3583fbc367e7280f568c1885be9bd1a01eb39
Atsushi Yokohama <yokohama@beachside.dev>
2022-08-14 15:29:44 +0000

    update

--- src/.vuepress/components/CertifiedLinks.vue
+++ src/.vuepress/components/CertifiedLinks.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="cert-container">
-    <a href="https://www.credly.com/badges/6eaa69f9-e04a-4bec-8ef7-2d0bbfa56959?source=linked_in_profile" target="_blank" rel="noopener">
+    <a href="https://www.credly.com/earner/earned/badge/9a6afd6c-e984-46ad-b227-9bae1825557f" target="_blank" rel="noopener">
+      <img src="images/azure-administrator-associate-600x600.png" alt="CERT image"/>
+    </a> 
+    <a href="https://www.credly.com/badges/6eaa69f9-e04a-4bec-8ef7-2d0bbfa56959" target="_blank" rel="noopener">
       <img src="images/azure-developer-associate-600x600.png" alt="CERT image"/>
     </a>
-    <a href="https://www.credly.com/badges/9f223842-3af8-44f2-85a8-21f36dac4061?source=linked_in_profile" target="_blank" rel="noopener">
+    <a href="https://www.credly.com/badges/9f223842-3af8-44f2-85a8-21f36dac4061" target="_blank" rel="noopener">
       <img src="images/CERT-Expert-DevOps-Engineer-600x600.png" alt="CERT image"/>
     </a>
-  </div>
+  </div>  
 </template>
 
 <style>
