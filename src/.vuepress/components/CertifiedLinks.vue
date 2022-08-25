1438576a8e0385390c8ab707aafc1ff1410869a4
Atsushi Yokohama <yokohama@beachside.dev>
2022-08-25 12:57:31 +0900

    Adding AZ-305

--- src/.vuepress/components/CertifiedLinks.vue
+++ src/.vuepress/components/CertifiedLinks.vue
@@ -1,15 +1,46 @@
 <template>
   <div class="cert-container">
-    <a href="https://www.credly.com/badges/9a6afd6c-e984-46ad-b227-9bae1825557f" target="_blank" rel="noopener">
-      <img src="images/azure-administrator-associate-600x600.png" alt="CERT image"/>
-    </a> 
-    <a href="https://www.credly.com/badges/6eaa69f9-e04a-4bec-8ef7-2d0bbfa56959" target="_blank" rel="noopener">
-      <img src="images/azure-developer-associate-600x600.png" alt="CERT image"/>
+    <a
+      href="https://www.credly.com/badges/9f223842-3af8-44f2-85a8-21f36dac4061"
+      target="_blank"
+      rel="noopener"
+    >
+      <img
+        src="images/CERT-Expert-DevOps-Engineer-600x600.png"
+        alt="CERT image"
+      />
     </a>
-    <a href="https://www.credly.com/badges/9f223842-3af8-44f2-85a8-21f36dac4061" target="_blank" rel="noopener">
-      <img src="images/CERT-Expert-DevOps-Engineer-600x600.png" alt="CERT image"/>
+    <a
+      href="https://www.credly.com/badges/b3d87ca2-bec8-4fd7-b13b-83a50105df7e/public_url"
+      target="_blank"
+      rel="noopener"
+    >
+      <img
+        src="images/azure-solutions-architect-expert-600x600.png"
+        alt="CERT image"
+      />
     </a>
-  </div>  
+    <a
+      href="https://www.credly.com/badges/9a6afd6c-e984-46ad-b227-9bae1825557f"
+      target="_blank"
+      rel="noopener"
+    >
+      <img
+        src="images/azure-administrator-associate-600x600.png"
+        alt="CERT image"
+      />
+    </a>
+    <a
+      href="https://www.credly.com/badges/6eaa69f9-e04a-4bec-8ef7-2d0bbfa56959"
+      target="_blank"
+      rel="noopener"
+    >
+      <img
+        src="images/azure-developer-associate-600x600.png"
+        alt="CERT image"
+      />
+    </a>
+  </div>
 </template>
 
 <style>
@@ -26,7 +57,6 @@ a {
 
 .cert-container img {
   width: 150px;
-  margin: 0px 15px ;
+  margin: 0px 15px;
 }
-
 </style>
