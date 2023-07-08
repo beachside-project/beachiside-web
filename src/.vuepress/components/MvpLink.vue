675f48bfbaa86e0272e1eccc5bd4e039dfad1a55
Atsushi YOKOHAMA <yokohama@beachside.dev>
2023-07-08 19:00:40 +0000

    Update

--- src/.vuepress/components/MvpLink.vue
+++ src/.vuepress/components/MvpLink.vue
@@ -1,26 +1,24 @@
 <template>
-    <div class="mvplink-wrapper">
-    <a
+  <div class="mvplink-wrapper">
+    <div style="margin-bottom:1rem">
+      <a
       href="https://www.credly.com/badges/2bbdd392-c2f2-4a42-a8f0-8e701249c945/public_url"
       target="_brank" rel="noopener"
-    >
+      >
       <img
-        src="https://www.credly.com/badges/2bbdd392-c2f2-4a42-a8f0-8e701249c945/embedded"
+        src="https://images.credly.com/size/680x680/images/5c687ffb-7ab6-4fd5-bf8c-14f0178acd21/image.png"
         alt="MVP official image"
       />
-    </a>
-  </div>
-  <div class="mvplink-wrapper">
-    <a
-      href="https://mvp.microsoft.com/ja-jp/PublicProfile/5002691?fullName=Atsushi%20Yokohama"
-      target="_brank" rel="noopener"
-    >
-      <img
-        src="images/MVP_Logo_Horizontal_Secondary_Blue288_CMYK_72ppi.png"
-        alt="MVP official image"
-      />
-    </a>
-    <p>Since 2017.</p>
+      </a>
+    </div>
+    <p>
+      <a
+        href="https://mvp.microsoft.com/ja-jp/PublicProfile/5002691?fullName=Atsushi%20Yokohama"
+        target="_brank" rel="noopener"
+      >
+      Since 2017.
+      </a>
+    </p>
   </div>
 </template>
 
